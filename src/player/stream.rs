b82148854dc0d4b9c6b7d95321a1f0c8e4c34c02
takashialpha <takashialpha@protonmail.com>
2024-12-19 23:34:56 -0300

    it builds, but with much warnings, and dont run as expected.

--- src/player/stream.rs
+++ src/player/stream.rs
@@ -1,5 +1,6 @@
 use cpal::{Device, SampleFormat, Stream, StreamConfig};
 use cpal::traits::{HostTrait, StreamTrait, DeviceTrait};
+use dasp_sample::{Sample, FromSample};
 use crate::error::error::AudioPlayerError;
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::Arc;
@@ -13,13 +14,20 @@ pub struct StreamHandler {
 impl StreamHandler {
     pub fn from_samples<T>(samples: Vec<T>) -> Result<Self, AudioPlayerError>
     where
-        T: cpal::Sample + Send + 'static,
+        T: cpal::Sample + Sample + Send + 'static,
+        i16: Sample<Float = T::Float> + FromSample<T>,
+        u8: Sample<Float = T::Float> + FromSample<T>,
+        f32: Sample<Float = T::Float> + FromSample<T>,
     {
         let host = cpal::default_host();
-        let device = host.default_output_device()
+        let device = host
+            .default_output_device()
             .ok_or(AudioPlayerError::StreamError("No output device available".to_string()))?;
-        let supported_config = device.default_output_config()
+        
+        let supported_config = device
+            .default_output_config()
             .map_err(|_| AudioPlayerError::StreamError("Error querying configs".to_string()))?;
+        
         let config = supported_config.config();
         let total_samples = samples.len();
         let cursor = Arc::new(AtomicUsize::new(0));
@@ -28,10 +36,12 @@ impl StreamHandler {
         let stream = match supported_config.sample_format() {
             SampleFormat::I16 => build_stream::<T, i16>(device, config, samples, stream_cursor),
             SampleFormat::U8 => build_stream::<T, u8>(device, config, samples, stream_cursor),
+            SampleFormat::F32 => build_stream::<T, f32>(device, config, samples, stream_cursor),
             _ => return Err(AudioPlayerError::StreamError("Unsupported sample format".to_string())),
-        };
+        }?;
 
         stream.play().map_err(|_| AudioPlayerError::StreamError("Failed to start the stream".to_string()))?;
+
         Ok(Self {
             stream,
             total_samples,
@@ -39,18 +49,23 @@ impl StreamHandler {
         })
     }
 
-    pub fn play(&self) {
-        self.stream.play().expect("Failed to play stream");
+    pub fn play(&self) -> Result<(), AudioPlayerError> {
+        self.stream
+            .play()
+            .map_err(|_| AudioPlayerError::StreamError("Failed to play stream".to_string()))
     }
 
-    pub fn pause(&self) {
-        self.stream.pause().expect("Failed to pause stream");
+    pub fn pause(&self) -> Result<(), AudioPlayerError> {
+        self.stream
+            .pause()
+            .map_err(|_| AudioPlayerError::StreamError("Failed to pause stream".to_string()))
     }
 
-    pub fn restart(&self) {
-        self.stream.pause().expect("Failed to pause stream");
+    pub fn restart(&self) -> Result<(), AudioPlayerError> {
+        self.pause()?;
         self.cursor.store(0, Ordering::Relaxed);
-        self.stream.play().expect("Failed to restart stream");
+        self.play()?;
+        Ok(())
     }
 
     pub fn progress(&self) -> f32 {
@@ -63,10 +78,10 @@ fn build_stream<T, O>(
     config: StreamConfig,
     audio_buffer: Vec<T>,
     cursor: Arc<AtomicUsize>,
-) -> Stream
+) -> Result<Stream, AudioPlayerError>
 where
-    O: cpal::Sample + cpal::SizedSample,
-    T: cpal::Sample + Send + 'static,
+    T: cpal::Sample + Sample + Send + 'static,
+    O: cpal::Sample + cpal::SizedSample + Sample + FromSample<T>,
 {
     let err_fn = |err| eprintln!("Error on audio stream: {}", err);
     
@@ -81,7 +96,7 @@ where
         }
     };
 
-    device.build_output_stream(&config, write_output, err_fn, None)
-        .expect("Failed to build output stream")
+    device
+        .build_output_stream(&config, write_output, err_fn, None)
+        .map_err(|_| AudioPlayerError::StreamError("Failed to build output stream".to_string()))
 }
-
